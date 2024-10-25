7de9af2172c44722570c53e7644a16f9bad9eaa3
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-10-25 15:01:38 +0200

    simple tests
    Former-commit-id: 82e0da268a8ee2a5712a25b8518d375236cf6d5b

---
+++ glimpse_generators/test_unreal_image_generator.py
@@ -0,0 +1,68 @@
+import random
+import cv2
+import numpy as np
+import pathlib
+
+from PIL import Image
+
+from glimpse_generators.unreal_glimpse_generator import UnrealGridGlimpseGenerator, UnrealGlimpseGenerator
+from response_parsers.xml_drone_response_parser import Direction
+from misc.cv2_and_numpy import opencv_to_pil, pil_to_opencv
+
+
+class TestUnrealImageGenerator:
+    def _move_by_prompt(self, rel_position, direction, distance):
+
+        if direction == Direction.NORTH:
+            return rel_position[0], rel_position[1] + distance, rel_position[2]
+        elif direction == Direction.SOUTH:
+            return rel_position[0], rel_position[1] - distance, rel_position[2]
+        elif direction == Direction.EAST:
+            return rel_position[0] + distance, rel_position[1], rel_position[2]
+        elif direction == Direction.WEST:
+            return rel_position[0] - distance, rel_position[1], rel_position[2]
+        elif direction == Direction.UP:
+            return rel_position[0], rel_position[1], rel_position[2] + distance
+        elif direction == Direction.DOWN:
+            return rel_position[0], rel_position[1], rel_position[2] - distance
+
+    def test_linear(self):
+        test_path = pathlib.Path("../all_logs/test_unreal_image_generator/linear_y")
+        test_path.mkdir(exist_ok=True)
+
+        generator = UnrealGlimpseGenerator(port=9000)
+
+        n = 100
+        min_y = -200
+        max_y = 200
+
+        start_x = 0
+        start_z = 100
+
+        for y in range(min_y, max_y, 10):
+            current_point = (start_x, y, start_z)
+            image = generator.get_camera_image(current_point)
+
+            image.save(test_path / f"{y}.png")
+
+    def test_linear_grid(self):
+        test_path = pathlib.Path("../all_logs/test_unreal_image_generator/linear_grid2")
+        test_path.mkdir(exist_ok=True)
+
+        generator = UnrealGridGlimpseGenerator(port=9000, splits_w=6, splits_h=6)
+
+        n = 100
+        min_z = 5
+        max_z = 120
+
+        start_x = -50
+        start_y = -55
+
+        for z in range(min_z, max_z, 5):
+            current_point = (start_x, start_y, z)
+            image = generator.get_camera_image(current_point)
+
+            image.save(test_path / f"{z}_0.png")
+
+            image = generator.get_camera_image((start_x + 5, start_y, z))
+            image.save(test_path / f"{z}_5.png")
