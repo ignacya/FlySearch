4195209160045c05fea93b6a6fb1547bc906cd2e
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-11-18 22:55:36 +0100

    another plot for timeseries
    Former-commit-id: e99d887cd382be8c657e12a7e7d97d055a991923

--- analysis/plot_timeseries.py
+++ analysis/plot_timeseries.py
@@ -29,8 +29,19 @@ def plot_badgrid_vs_newgrid(ax):
     plot_time_series("../all_logs/newgrid-gpt-3", ax[1], "GPT-4o, directly above, newgrid")
     plot_time_series("../all_logs/newgrid-gpt-11-closer-incontext", ax[2], "GPT-4o, directly above, newgrid, forcefully winning the argument")
 
+def plot_politeness(ax):
+    plot_time_series("../all_logs/newgrid-gpt-14-corner-politealt", ax[0], "GPT-4o, corner, polite + altitude")
+    plot_time_series("../all_logs/newgrid-gpt-15-center-politealt", ax[1], "GPT-4o, center, polite + altitude")
+
 
 def main():
+    fig, axs = plt.subplots(1, 2, sharex=True, sharey=True)
+
+    plot_politeness(axs)
+    plt.show()
+
+    exit()
+
     fig, axs = plt.subplots(1, 3, sharex=True, sharey=True)
 
     plot_closer_prompting_strategies(axs)
