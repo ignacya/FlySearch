31a98008c9b18adfa487a4fc31a7c2247a1eaeb2
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-12-03 14:26:39 +0100

    mapping
    Former-commit-id: 2ece7584e9889d25cbdb6e11d643e77afdca89bc

---
+++ scenarios/classes_to_ids.py
@@ -0,0 +1,13 @@
+from scenarios.forest_scenario_mapper import ForestScenarioMapper
+
+classes_to_ids = {
+    ForestScenarioMapper.ObjectType.FIRE: "NiagaraActor_UAID_08BFB8191750822F02_1834612234",
+    ForestScenarioMapper.ObjectType.TRASH: "BP_Configuration_1_C_UAID_08BFB8191750822F02_1638429225",
+    ForestScenarioMapper.ObjectType.TENT: "StaticMeshActor_UAID_08BFB8191750812F02_1554258022",
+    ForestScenarioMapper.ObjectType.BUILDING: "StaticMeshActor_UAID_08BFB8191750802F02_1934372840",
+    ForestScenarioMapper.ObjectType.PERSON: "SkeletalMeshActor_UAID_08BFB8191750A22F02_1368103644",
+    ForestScenarioMapper.ObjectType.UFO: "StaticMeshActor_UAID_08BFB8191750683002_1841587498",
+    ForestScenarioMapper.ObjectType.PLANE: "StaticMeshActor_UAID_08BFB8191750963002_1512367583",
+    ForestScenarioMapper.ObjectType.HELICOPTER: "StaticMeshActor_UAID_08BFB8191750683002_1545951497"
+}
+
