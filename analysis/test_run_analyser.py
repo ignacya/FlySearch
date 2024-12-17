4bb615a1da4777c30c3e9e2ffc24d65156a8a2a1
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-12-17 18:06:16 +0100

    added tests for run analyser
    Former-commit-id: 714d5344f1882bb279b60efb2921549983c5974d

---
+++ analysis/test_run_analyser.py
@@ -0,0 +1,49 @@
+from analysis.run_analyser import RunAnalyser
+
+
+class TestRunAnalyser:
+    class MockRun:
+        def __init__(self, end_position, object_bbox):
+            self.coords = [end_position]
+            self.object_bbox = object_bbox
+
+        def get_coords(self):
+            return self.coords
+
+        def get_object_bbox(self):
+            return self.object_bbox
+
+        @property
+        def end_position(self):
+            return self.coords[-1]
+
+    def test_object_visible(self):
+        run = self.MockRun((0, 0, 100), (-1000, -1000, 0, 1000, 1000, 300))
+
+        run_analyser = RunAnalyser(run)
+
+        assert run_analyser.object_visible()
+
+    def test_object_within_altitude_threshold(self):
+        run = self.MockRun((0, 0, 100), (-1000, -1000, 0, 1000, 1000, 9000))
+
+        run_analyser = RunAnalyser(run)
+
+        assert run_analyser.drone_within_altitude_threshold(10)
+        assert run_analyser.maciek_criterion_satisfied(10)
+
+    def test_object_offset(self):
+        run = self.MockRun((-100, 100, 101), (-30000, -5000, -500, -300, 1000, 9500))
+
+        run_analyser = RunAnalyser(run)
+
+        assert run_analyser.object_visible()
+        assert run_analyser.drone_within_altitude_threshold(10)
+        assert run_analyser.maciek_criterion_satisfied(10)
+
+    def test_euclidean_distance_to_zero(self):
+        run = self.MockRun((0, 3, 4), (-1000, -1000, 0, 1000, 1000, 300))
+
+        run_analyser = RunAnalyser(run)
+
+        assert run_analyser.get_euclidean_distance() == 5
