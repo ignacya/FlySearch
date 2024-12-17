0588e94159c5019311dbe03d69c0f4b3e6c9694c
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-12-17 12:55:17 +0100

    fixed test run directory
    Former-commit-id: 1270d6b0704bbf80518e14c935beb8bca02c602b

--- analysis/test_run.py
+++ analysis/test_run.py
@@ -6,7 +6,7 @@ from analysis.run import Run
 class TestRun:
     def test_coordinates_are_loaded_correctly(self):
         print(os.getcwd())
-        base_path = pathlib.Path("/home/dominik/MyStuff/active-visual-gpt/all_logs/MC-0S-F")
+        base_path = pathlib.Path("../all_logs/MC-0S-F")
         runs = sorted(os.listdir(base_path), key=lambda x: int(x.split("_")[0]))
 
         run = Run(base_path / runs[0])
@@ -18,7 +18,7 @@ class TestRun:
         assert coords[1] == (2.0, 4.0, 9.0)
 
     def test_params_are_loaded_correctly(self):
-        base_path = pathlib.Path("/home/dominik/MyStuff/active-visual-gpt/all_logs/MC-0S-F")
+        base_path = pathlib.Path("../all_logs/MC-0S-F")
         runs = sorted(os.listdir(base_path), key=lambda x: int(x.split("_")[0]))
 
         run = Run(base_path / runs[1])
