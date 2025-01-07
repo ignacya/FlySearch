0b48d52a4bc103ca80ac567649efcf4c8464a957
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2025-01-07 22:13:43 +0100

    slight optimisation
    Former-commit-id: c72feac2abeda3024b51389fd17df593de7680b0

--- scenarios/object_classes/base_object_class.py
+++ scenarios/object_classes/base_object_class.py
@@ -8,6 +8,7 @@ class BaseObjectClass:
     def __init__(self, spawnable_object_ids: List[str], client: Client):
         self.spawnable_object_ids = spawnable_object_ids
         self.client = client
+        self.visible = True
 
     def _hide_object(self, object_id: str):
         self.client.request(f"vset /objects/{object_id}/hide")
@@ -16,11 +17,16 @@ class BaseObjectClass:
         self.client.request(f"vset /objects/{object_id}/show")
 
     def hide_all_objects(self):
+        if not self.visible:
+            return
+
         for object_id in self.spawnable_object_ids:
             self._hide_object(object_id)
 
     # Returns object id of the object moved
     def move_and_show(self, x: float, y: float, z: float, seed: int) -> str:
+        self.visible = True
+
         rng = Random()
         rng.seed(seed)
         object_id = rng.choice(self.spawnable_object_ids)
