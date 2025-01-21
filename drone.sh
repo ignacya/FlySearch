e951fe6aead8c1da0e6741b3c1560d24d24b26cf
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2025-01-21 13:37:19 +0100

    exporting font location
    Former-commit-id: 62e1a700a1e5003f8b4c098678ba9121b451871a

--- drone.sh
+++ drone.sh
@@ -1,13 +1,14 @@
 export CITY_BINARY_PATH="/home/dominik/MyStuff/simulator/CitySample/Binaries/Linux/CitySample"
 export FOREST_BINARY_PATH="/home/dominik/MyStuff/simulator-dreamsenv/Linux/ElectricDreamsEnv/Binaries/Linux/ElectricDreamsSample"
 export LOCATIONS_CITY_PATH="/home/dominik/MyStuff/active-visual-gpt/locations_city.csv"
+export FONT_LOCATION="/usr/share/fonts/google-noto/NotoSerif-Bold.ttf"
 
 python3 drone.py \
 --prompt xml_grid_grid_found \
 --glimpses 5 \
 --glimpse_generator grid \
 --model gpt-4o \
---run_name A7 \
+--run_name A8 \
 --scenario_type city \
 --navigator grid \
 --height_min 30 \
