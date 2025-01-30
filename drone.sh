1536c9219ee5333ae5dec0db31ba095f48ab4fb2
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2025-01-30 23:40:00 +0100

    anonymised drone.sh
    Former-commit-id: 21abc6fae0dd4ad69d52bbf72a9d8b38f9fbac01

--- drone.sh
+++ drone.sh
@@ -1,21 +1,22 @@
-export CITY_BINARY_PATH="/home/dominik/MyStuff/simulator/CitySample/Binaries/Linux/CitySample"
-export FOREST_BINARY_PATH="/home/dominik/MyStuff/simulator-dreamsenv/Linux/ElectricDreamsEnv/Binaries/Linux/ElectricDreamsSample"
-export LOCATIONS_CITY_PATH="/home/dominik/MyStuff/active-visual-gpt/locations_city.csv"
+export CITY_BINARY_PATH="/yourpath/simulator/CitySample/Binaries/Linux/CitySample"
+export FOREST_BINARY_PATH="/yourpath/simulator-dreamsenv/Linux/ElectricDreamsEnv/Binaries/Linux/ElectricDreamsSample"
+export LOCATIONS_CITY_PATH="/yourpath/locations_city.csv"
 export FONT_LOCATION="/usr/share/fonts/google-noto/NotoSerif-Bold.ttf"
 
 python3 drone.py \
 --prompt xml_grid_grid_found \
---glimpses 5 \
+--glimpses 10 \
 --glimpse_generator grid \
---model gpt-4o \
---run_name A11 \
---scenario_type city \
+--model anthropic-claude-3-5-sonnet-20241022 \
+--run_name SomeRun \
+--scenario_type forest \
 --navigator grid \
 --height_min 30 \
 --height_max 100 \
 --incontext False \
 --repeats 1 \
 --logdir all_logs \
---n 1
-
-
+--n 200 \
+--mimic_run_name forest-template \
+--mimic_run_cls_names "*" \
+#--continue_from 217
