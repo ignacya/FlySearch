7c0318932df17af39505add16a9c31b299785e95
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2025-04-03 11:07:13 +0200

    server environment
    Former-commit-id: 11067b625b6ff2e35fc749d9e4267972e2508b9f

---
+++ web/run_webserver.sh
@@ -0,0 +1,6 @@
+export CITY_BINARY_PATH="/home/dominik/MyStuff/simulator/CitySample/Binaries/Linux/CitySample"
+export FOREST_BINARY_PATH="/home/dominik/MyStuff/simulator-dreamsenv/Linux/ElectricDreamsEnv/Binaries/Linux/ElectricDreamsSample"
+export LOCATIONS_CITY_PATH="/home/dominik/MyStuff/active-visual-gpt/locations_city.csv"
+export FONT_LOCATION="/usr/share/fonts/google-noto/NotoSerif-Bold.ttf"
+
+uvicorn web:app:app
