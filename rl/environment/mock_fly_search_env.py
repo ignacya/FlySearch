a5c7860aaf06229fc8095ce77aa8d30f7566cb23
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2025-04-15 20:30:03 +0200

    better mock
    Former-commit-id: 7deaf29de2d71eb659d7b93211a754075bd68961

--- rl/environment/mock_fly_search_env.py
+++ rl/environment/mock_fly_search_env.py
@@ -17,23 +17,30 @@ class MockClient(UnrealClientWrapper):
 
 
 class MockGlimpseGenerator(UnrealGridGlimpseGenerator):
-    def __init__(self):
-        pass
+    def __init__(self, start_unreal_coords=(50000.0, 50000.0, 1000.0), current_relative_coords=(0.0, 0.0, 0.0)):
+        self.start_unreal_coords = start_unreal_coords
+        self.current_relative_coords = current_relative_coords
 
     def get_relative_from_start(self):
-        return 42, 42, 42
+        return [
+            int(self.current_relative_coords[0]),
+            int(self.current_relative_coords[1]),
+            int(self.current_relative_coords[2]),
+        ]
 
     def get_camera_image(self, rel_position_m=(0, 0, 0), force_move=False):
-        image = cv2.imread("/home/dominik/MyStuff/active-visual-gpt/data/burger.png")
+        image = cv2.imread("/home/dominik/MyStuff/active-visual-gpt/data/golden_retriever.png")
         image = cv2.resize(image, (512, 512))
         image = cv2.resize(image, (500, 500))
+
+        self.current_relative_coords = rel_position_m
 
         return image
 
 
 class MockFlySearchEnv(BaseFlySearchEnv):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def get_client(self):
         return MockClient()
