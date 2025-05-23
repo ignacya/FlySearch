c131db0e985195d271571fcd1c7f3e984fc46325
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2025-05-23 23:20:14 +0200

    agent factory resolver
    Former-commit-id: 9a24c8609e743ba50a1381a1f2ee2af5af9584b0

--- arg_resolvers/agent_factory_resolver.py
+++ arg_resolvers/agent_factory_resolver.py
@@ -1,11 +1,12 @@
 from arg_resolvers import BaseArgResolver
 
-from rl.agents import SimpleLLMAgentFactory, DescriptionLLMAgentFactory
+from rl.agents import SimpleLLMAgentFactory, DescriptionLLMAgentFactory, GeneralistOneAgentFactory
 
 
 class AgentFactoryResolver(BaseArgResolver):
     def register_args(self, parser):
-        parser.add_argument("--agent", type=str, required=True, choices=["simple_llm", "description_llm"])
+        parser.add_argument("--agent", type=str, required=True,
+                            choices=["simple_llm", "description_llm", "generalist_one"], )
 
     def resolve_args(self, args, accumulator):
         if args.agent == "simple_llm":
@@ -14,6 +15,10 @@ class AgentFactoryResolver(BaseArgResolver):
             )
         elif args.agent == "description_llm":
             agent_factory = DescriptionLLMAgentFactory(
+                conversation_factory=accumulator["conversation_factory"],
+            )
+        elif args.agent == "generalist_one":
+            agent_factory = GeneralistOneAgentFactory(
                 conversation_factory=accumulator["conversation_factory"],
             )
         else:
