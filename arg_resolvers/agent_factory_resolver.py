27adaaf48008d7edc4f816e21491e066d3491464
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2025-05-30 14:34:48 +0200

    agent factory
    Former-commit-id: 9fde57b277119cfa8281343feff2294bc0e3c855

--- arg_resolvers/agent_factory_resolver.py
+++ arg_resolvers/agent_factory_resolver.py
@@ -1,12 +1,14 @@
 from arg_resolvers import BaseArgResolver
 
-from rl.agents import SimpleLLMAgentFactory, DescriptionLLMAgentFactory, GeneralistOneAgentFactory
+from rl.agents import SimpleLLMAgentFactory, DescriptionLLMAgentFactory, GeneralistOneAgentFactory, \
+    DetectionDrivenDescriptionLLMAgentFactory, DetectionCheaterFactory
 
 
 class AgentFactoryResolver(BaseArgResolver):
     def register_args(self, parser):
         parser.add_argument("--agent", type=str, required=True,
-                            choices=["simple_llm", "description_llm", "generalist_one"], )
+                            choices=["simple_llm", "description_llm", "generalist_one",
+                                     "detection_driven_description_llm", "detection_cheater_factory"], )
 
     def resolve_args(self, args, accumulator):
         if args.agent == "simple_llm":
@@ -19,6 +21,14 @@ class AgentFactoryResolver(BaseArgResolver):
             )
         elif args.agent == "generalist_one":
             agent_factory = GeneralistOneAgentFactory(
+                conversation_factory=accumulator["conversation_factory"],
+            )
+        elif args.agent == "detection_driven_description_llm":
+            agent_factory = DetectionDrivenDescriptionLLMAgentFactory(
+                conversation_factory=accumulator["conversation_factory"],
+            )
+        elif args.agent == "detection_cheater_factory":
+            agent_factory = DetectionCheaterFactory(
                 conversation_factory=accumulator["conversation_factory"],
             )
         else:
